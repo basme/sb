d04f007e2c9024b44f55b7152e7c9e285f4b2967
basme <bashmakov.dan@gmail.com>
2022-07-27 00:09:00 +0400

    application stub and gitignore changes

---
+++ src/main/java/com/daba/sb/Application.java
@@ -0,0 +1,14 @@
+package com.daba.sb;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Application {
+
+    public static void main(String[] args) {
+        log.info("Application is started");
+        
+        log.trace("Application is finished");
+    }
+
+}
