03a5828a6e5b029bf87942b35a586f0db0d9783c
basme <bashmakov.dan@gmail.com>
2022-07-27 00:20:22 +0400

    basic classes

--- src/main/java/com/daba/sb/Application.java
+++ src/main/java/com/daba/sb/Application.java
@@ -7,7 +7,8 @@ public class Application {
 
     public static void main(String[] args) {
         log.info("Application is started");
-        
+        Game game = new Game();
+        game.play();
         log.trace("Application is finished");
     }
 
---
+++ src/main/java/com/daba/sb/Game.java
@@ -0,0 +1,8 @@
+package com.daba.sb;
+
+public class Game {
+
+    public void play() {
+            }
+
+}
